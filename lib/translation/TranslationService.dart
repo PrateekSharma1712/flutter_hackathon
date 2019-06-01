c40ad269ef8f933f92b0a30d7f6df6f47306b06f
Amit Wani <mtwn105@gmail.com>
2019-06-01 12:52:52 +0530

    implemented Translation Service & Translation Response Model

---
+++ lib/translation/TranslationService.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:flutter_hackathon/translation/TranslationResponse.dart';
+import 'package:http/http.dart' as http;
+
+String apiKey = "AIzaSyBg-EiyCyYqIFOffHqHhgkrm4ILJSl2V4I";
+
+Future<Translations> translateString(String source, String languageCode) async {
+  String url =
+      "https://translation.googleapis.com/language/translate/v2?target=${languageCode}&key=${apiKey}&q=${source}";
+
+  var response = await http.get(url);
+  //print(response.body);
+  var jsonResponse = json.decode(response.body);
+  TranslationResponse translationResponse =
+      TranslationResponse.fromJson(jsonResponse);
+  Translations translation = translationResponse.data.translations[0];
+  return translation;
+}
