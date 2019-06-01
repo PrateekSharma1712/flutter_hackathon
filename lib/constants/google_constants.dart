531f1c3f04c844ed6e4072be01d7bec4eb9acee4
Amit Wani <mtwn105@gmail.com>
2019-06-01 13:16:14 +0530

    added trnslation api key to constants

--- lib/constants/google_constants.dart
+++ lib/constants/google_constants.dart
@@ -1,3 +1,4 @@
 class GoogleConstants {
   static const GOOGLE_CLIENT_KEY = "7b541c8e2b6d43749aeca04bc9bdc66e";
+  static const TRANSLATION_API_KEY = "AIzaSyBg-EiyCyYqIFOffHqHhgkrm4ILJSl2V4I";
 }
--- lib/translation/TranslationService.dart
+++ lib/translation/TranslationService.dart
@@ -1,13 +1,11 @@
 import 'dart:convert';
-
+import 'package:flutter_hackathon/constants/google_constants.dart';
 import 'package:flutter_hackathon/translation/TranslationResponse.dart';
 import 'package:http/http.dart' as http;
 
-String apiKey = "AIzaSyBg-EiyCyYqIFOffHqHhgkrm4ILJSl2V4I";
-
 Future<Translation> translateString(String source, String languageCode) async {
   String url =
-      "https://translation.googleapis.com/language/translate/v2?target=${languageCode}&key=${apiKey}&q=${source}";
+      "https://translation.googleapis.com/language/translate/v2?target=${languageCode}&key=${GoogleConstants.TRANSLATION_API_KEY}&q=${source}";
 
   var response = await http.get(url);
   //print(response.body);
